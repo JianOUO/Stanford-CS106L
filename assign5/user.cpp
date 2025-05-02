c8e26dc5e01cea84f4ad4eeb75b1dca60d1004a0
21009100537-Jin An <1184880285@qq.com>
2025-05-02 15:55:58 +0800

    assign5 pass

--- assign5/user.cpp
+++ assign5/user.cpp
@@ -63,3 +63,54 @@ void User::set_friend(size_t index, const std::string& name)
  * STUDENT TODO:
  * The definitions for your custom operators and special member functions will go here!
  */
+std::ostream& operator<< (std::ostream& out, const User& user) 
+{
+  out << "User(name=" << user._name << ", friends=[";
+  if (user._size > 0) {
+    for (size_t i = 0; i < user._size - 1; i++) {
+      out << user._friends[i] << ", ";
+    }
+    out << user._friends[user._size - 1];
+  }
+  out << "])";
+  return out;
+}
+
+User::~User() {
+  delete[] _friends;
+}
+User::User(const User& user) : _name(user._name), _size(user._size), _capacity(user._capacity), _friends(new std::string[user._capacity]) {
+  std::copy(user._friends, user._friends + user._size, _friends);
+}
+User& User::operator= (const User& user) {
+  if (this == &user) return *this;
+  delete[] _friends;
+  _name = user._name;
+  _size = user._size;
+  _capacity = user._capacity;
+  _friends = new std::string[_capacity];
+  std::copy(user._friends, user._friends + _size, _friends);
+  return *this;
+}
+User& User::operator+= (User& rhs) {
+  add_friend(rhs._name);
+  rhs.add_friend(_name);
+  return *this;
+}
+bool User::operator<(const User& rhs) const {
+  return _name < rhs._name;
+}
+/*
+User::User(User&& user) : _name(user._name), _size(user._size), _capacity(user._capacity), _friends(user._friends) {
+  user._friends = nullptr;
+}
+User& User::operator= (User&& user) {
+  delete[] _friends; 
+  _name = user._name;
+  _size = user._size;
+  _capacity = user._capacity;
+  _friends = user._friends;
+  user._friends = nullptr;
+  return *this;
+}
+*/
